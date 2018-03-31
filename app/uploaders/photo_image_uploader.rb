35d64be061582a581c776bb7327d9ebd7ae6e460
anna770822 <anna770822@hotmail.com>
2018-03-31 19:45:32 +0800

    change directory

--- app/uploaders/photo_image_uploader.rb
+++ app/uploaders/photo_image_uploader.rb
@@ -11,7 +11,7 @@ class PhotoImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    'http://s3.amazonaws.com/bucketname/photoalbum'
+    'http://s3.amazonaws.com/bucketname/photoalbum-assets'
   end
 
 
