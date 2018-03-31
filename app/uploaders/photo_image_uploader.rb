e665cb84bf0af6c1a77c2d1c87180e066b815234
anna770822 <anna770822@hotmail.com>
2018-03-31 19:28:07 +0800

    add AW3

--- app/uploaders/photo_image_uploader.rb
+++ app/uploaders/photo_image_uploader.rb
@@ -11,8 +11,9 @@ class PhotoImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    'http://s3.amazonaws.com/bucketname/photoalbum'
   end
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
